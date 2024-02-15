4a7a38dba8973e23f86bd925614d8ea349835faa
Yuta <yuta.marines.team26l@gmail.com>
2024-02-16 08:42:03 +0900

    models.pyにテーブルスキーマを記載

--- database/models.py
+++ database/models.py
@@ -1,15 +1,18 @@
 # データベーススキーマを定義するためのファイルで、FlaskのORM（Object Relational Mapping）ツールであるSQLAlchemyを使用してモデルを定義。
 # モデルはデータベーステーブルの各行を表すクラス。
-# 以下は仮なので要改変
 
-# models.pyのコードは SQLAlchemy ORM (Object Relational Mapper) の使用を示しており、これはデータベース操作をより直観的でPythonらしいコードで行うためのものです。
-# データベースとテーブルが既に作成されている場合でも、アプリケーション内からデータベースを操作するためにはこのようなモデル定義が必要になります。
+# models.pyのコードは SQLAlchemy ORM (Object Relational Mapper) の使用を示しており、これはデータベース操作をより直観的でPythonらしいコードで行うためのもの。
+# データベースとテーブルが既に作成されている場合でも、アプリケーション内からデータベースを操作するためにはこのようなモデル定義が必要になる。
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date, DECIMAL
 from .database import Base
 
-class User(Base):
-    __tablename__ = "users"
+class AdxLast7daysData(Base):
+    __tablename__ = "Adx_Last_7days_Data"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    date = Column(Date)
+    ad_unit = Column(String(255))
+    adx_impressions = Column(Integer)
+    adx_revenue = Column(DECIMAL(10, 2))
+    avg_adx_cpm = Column(DECIMAL(10, 2))
