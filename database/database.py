7bf93b0e6da6381c9a8eb635c065088954def1e3
Yuta <yuta.marines.team26l@gmail.com>
2024-02-16 09:57:08 +0900

    .envファイルの作成

--- database/database.py
+++ database/database.py
@@ -6,17 +6,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-user = os.getenv("MYSQL_USER")
-password = os.getenv("MYSQL_PASSWORD")
-host = os.getenv("MYSQL_HOST", "localhost")  # optional: "localhost" is a default value
-dbname = os.getenv("MYSQL_DBNAME")
+user = os.environ['MYSQL_USER']
+password = os.environ['MYSQL_PASSWORD']
+host = os.environ['MYSQL_HOST']
+dbname = os.environ['MYSQL_DATABASE']
 
-# ソースコード上にパスワード等を直接表記しない
-# ターミナル上で以下の環境変数設定が必要
-# export MYSQL_USER='username'
-# export MYSQL_PASSWORD='password'
-# export MYSQL_HOST='localhost'
-# export MYSQL_DBNAME='dbname'
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{user}:{password}@{host}/{dbname}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
