55e9ee34ddd96964fea5e7cde1862d32219c4fb3
Yuta <yuta.marines.team26l@gmail.com>
2024-02-20 01:07:27 +0900

    selectbuttonの変更で別dataを表示できるようにした

--- fronted/src/App.tsx
+++ fronted/src/App.tsx
@@ -1,11 +1,51 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import Highcharts from 'highcharts';
+import Highcharts, { SeriesLineOptions } from 'highcharts';
 
 function App() {
-  const [selectedOption, setSelectedOption] = useState("default");
+  //useStateでoptionを定義
+  const [selectedOption, setSelectedOption] = useState("Overlay");
+
+
+  //useEffectでoptionが変更されるとHighChartsにて再描画
   useEffect(() => {
-    //highchartsにて描画
+    let selectedSeriesData: Highcharts.SeriesLineOptions[] = []; // ここで型を定義します。
+
+    if (selectedOption === 'Overlay') {
+      selectedSeriesData = [{
+        type: 'line', // 追加
+        name: '広告枠A',
+        data: [75, 82, 80, 76, 72, 70, 74, 79, 77, 81, 73, 75]
+      },
+      {
+        type: 'line', // 追加
+        name: '広告枠B',
+        data: [65, 68, 74, 71, 73, 66, 80, 71, 69, 75, 71, 77]
+      }];
+    } else if (selectedOption === 'Interstitial') {
+      selectedSeriesData = [{
+        type: 'line', // 追加
+        name: '広告枠C',
+        data: [92, 79, 101, 89, 90, 92, 88, 81, 67, 74, 69, 89]
+      },
+      {
+        type: 'line', // 追加
+        name: '広告枠D',
+        data: [78, 90, 78, 78, 84, 86, 89, 90, 87, 67, 77, 88]
+      }];
+    } else if (selectedOption === 'Inarticle') {
+      selectedSeriesData = [{
+        type: 'line', // 追加
+        name: '広告枠E',
+        data: [76, 67, 89, 90, 101, 110, 99, 97, 100, 104, 100, 102]
+      },
+      {
+        type: 'line', // 追加
+        name: '広告枠F',
+        data: [88, 89, 99, 78, 100, 100, 75, 88, 81, 78, 79, 90]
+      }];
+    }
+
     const chart = new Highcharts.Chart({
       chart: {
         renderTo: 'container',
@@ -40,23 +80,15 @@ function App() {
         y: 100,
         borderWidth: 0
       },
-      series: [{
-        type: 'line', // 追加
-        name: '広告枠A',
-        data: [75, 82, 80, 76, 72, 70, 74, 79, 77, 81, 73, 75]
-      },
-      {
-        type: 'line', // 追加
-        name: '広告枠B',
-        data: [65, 68, 74, 71, 73, 66, 80, 71, 69, 75, 71, 77]
-      }]
+      series: selectedSeriesData
     });
   }, [selectedOption]);
 
   // selectエレメントが変更されたときにselectedOptionを更新
   const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedOption(event.target.value);
-};
+    console.log(`${event.target.value} was selected.`)
+  };
 
   return (
     <>
@@ -64,9 +96,9 @@ function App() {
       </div>
       <div className="center">
         <select value={selectedOption} onChange={handleChange}>
-          <option value="option1">Overlay</option>
-          <option value="option2">Interstitial</option>
-          <option value="option3">Inarticle</option>
+          <option value="Overlay">Overlay</option>
+          <option value="Interstitial">Interstitial</option>
+          <option value="Inarticle">Inarticle</option>
         </select>
       </div>
     </>
