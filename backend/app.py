831623562b0bb7deac240dae5b18d787d29ce517
Yuta <yuta.marines.team26l@gmail.com>
2024-03-03 19:56:22 +0900

    認証に必要なライブラリをインストール、OAuthのクライアントIDとシークレットを.envにて指定。

--- backend/app.py
+++ backend/app.py
@@ -5,8 +5,12 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from flask import Flask, jsonify, redirect, request
 from backend import crud
 from flask_cors import CORS
+from flask_login import LoginManager
+from requests_oauthlib import OAuth2Session
 
 app = Flask(__name__)
+login_manager = LoginManager(app)
+oauth = OAuth2Session('GOOGLE_CLIENT_ID', redirect_uri='http://localhost:5000/authentication', scope='https://www.googleapis.com/auth/userinfo.email', client_secret='GOOGLE_CLIENT_SECRET')
 CORS(app)
 
 # ルーティングもここで書く
@@ -26,9 +30,10 @@ def handle_option(option):
 
 @app.route('/authentication')
 def authenticate():
-    code = request.args.get('code')  # Googleからの応答から認証コードを取得
-    # ここにアクセストークンの取得と利用に関する処理を書く
-    return redirect('http://localhost:3000/')
+    code = request.args.get('code')
+    token = oauth.fetch_token('https://oauth2.googleapis.com/token', authorization_response=request.url, client_secret='GOOGLE_CLIENT_SECRET', code=code)
+    # ここから先はアクセストークンを使った処理を書くことができます。
+    return redirect('http://localhost:5000/')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
