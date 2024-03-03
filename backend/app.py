371f02e673b7ecfa797c166ee4d2c0e468a5b8c1
Yuta <yuta.marines.team26l@gmail.com>
2024-03-03 19:46:35 +0900

    Google OAuthのリダイレクトURIをルーティング

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect, request
 from backend import crud
 from flask_cors import CORS
 
@@ -24,6 +24,11 @@ def handle_option(option):
     data = crud.get_data(option)
     return jsonify(data)
 
+@app.route('/authentication')
+def authenticate():
+    code = request.args.get('code')  # Googleからの応答から認証コードを取得
+    # ここにアクセストークンの取得と利用に関する処理を書く
+    return redirect('http://localhost:3000/')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
