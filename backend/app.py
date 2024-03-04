fd19e480c16f179641d59296d3cbcd63016bdcc9
Yuta <yuta.marines.team26l@gmail.com>
2024-03-05 03:13:16 +0900

    簡易的なログイン機能の完成

--- backend/app.py
+++ backend/app.py
@@ -9,19 +9,19 @@ os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 client_id = os.getenv('GOOGLE_CLIENT_ID')
 client_secret = os.getenv('GOOGLE_CLIENT_SECRET')
 
-from flask import Flask, jsonify, redirect, request
+from flask import Flask, jsonify, redirect, request, session
 from backend import crud
 from flask_cors import CORS
 from flask_login import LoginManager
 from requests_oauthlib import OAuth2Session
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY')
 login_manager = LoginManager(app)
-oauth = OAuth2Session(client_id, redirect_uri='http://localhost:5000/authentication', scope='openid https://www.googleapis.com/auth/userinfo.email')
-CORS(app)
+oauth = OAuth2Session(client_id, redirect_uri='http://localhost:5000/token', scope='openid https://www.googleapis.com/auth/userinfo.email')
+CORS(app, supports_credentials=True)
 
 # ルーティングもここで書く
-
 @app.route('/')
 def hello_world():
     return 'ここにGAMトレンドを表示したいかもね'
@@ -30,11 +30,13 @@ def hello_world():
 def sample():
     return 'ここはsampleページです'
 
+# overlay / interstitial のデータを取得するエンドポイント
 @app.route('/api/<option>', methods=['GET'])
 def handle_option(option):
     data = crud.get_data(option)
     return jsonify(data)
 
+# Google OAuth2.0を利用するエンドポイント
 @app.route('/login')
 def login():
     authorization_url, state = oauth.authorization_url(
@@ -43,15 +45,38 @@ def login():
         prompt="select_account")
     return redirect(authorization_url)
 
-@app.route('/authentication')
-def authenticate():
+# ログアウト時のエンドポイント
+@app.route('/logout')
+def logout():
+    session.pop('token', None)
+    return {"loggedIn": False}
+
+# Google OAuth2.0のリダイレクトURI, tokenを作成してホーム画面にリダイレクト
+@app.route('/token')
+def token():
+    code = request.args.get('code')
     try:
-        code = request.args.get('code')
-        token = oauth.fetch_token('https://oauth2.googleapis.com/token', authorization_response=request.url, client_secret=client_secret, code=code)
-        # ここから先はアクセストークンを使った処理を書くことができます。
-        return redirect('http://localhost:3000/')
+        token = oauth.fetch_token(
+            'https://oauth2.googleapis.com/token',
+            authorization_response=request.url,
+            client_secret=client_secret, 
+            code=code
+        )
+        # トークンをセッションに保存
+        session['token'] = token  
+        return redirect("http://localhost:3000")
     except Exception as e:
-        return "ログインに失敗しました: " + str(e)
+        return "トークンの取得に失敗しました: " + str(e)
+
+# ログイン状態を保持するエンドポイント
+@app.route('/loggedin')
+def loggedin():
+    # セッションからトークンを取得し、ログイン状態をチェック
+    if 'token' in session:
+        return {"loggedIn": True}
+    else:
+        return {"loggedIn": False}
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,88 +1,120 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import Highcharts, { SeriesLineOptions } from 'highcharts';
+import React, { useEffect, useRef, useState } from 'react';
+import Highcharts from 'highcharts';
 import axios from 'axios';
 
 function App() {
-  //useStateでoptionを定義
+  const [loggedIn, setLoggedIn] = useState(false);
   const [selectedOption, setSelectedOption] = useState("Overlay");
+  const containerRef = useRef(null);
+  const [chart, setChart] = useState<Highcharts.Chart | null>(null);
 
-
-  //useEffectでoptionが変更されるとHighChartsにて再描画
   useEffect(() => {
-    console.log(`${selectedOption} was selected.`)
     const fetchData = async () => {
       try {
         const response = await axios.get(`http://localhost:5000/api/${selectedOption.toLowerCase()}`);
-        
-        //JSONデータをHighchartsに適した形式に変換します
+
         const formattedData = response.data.map((item: { avg_cpm: string, date: string }) => {
           return {
-            x: new Date(item.date), // 日付
-            y: Number(item.avg_cpm) // CPM値
+            x: new Date(item.date),
+            y: Number(item.avg_cpm)
           }
         });
-  
-        var chart = new Highcharts.Chart({
-          chart: {
-            renderTo: 'container',
-            margin: [50, 150, 60, 80]
-          },
-          title: { text: undefined },
-          xAxis: {
-            type: 'datetime',
-            title: { text: null }
-          },
-          yAxis: {
-            title: { text: null },
-            plotLines: [{
-              value: 0,
-              width: 1,
-              color: '#808080'
+
+        // containerRef.currentが存在しているかどうかチェック
+        if (containerRef.current) {
+          const newChart = new Highcharts.Chart({
+            chart: {
+              renderTo: containerRef.current,
+              margin: [50, 150, 60, 80]
+            },
+            title: { text: undefined },
+            xAxis: {
+              type: 'datetime',
+              title: { text: null }
+            },
+            yAxis: {
+              title: { text: null },
+              plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+              }]
+            },
+            tooltip: {
+              pointFormat: 'CPM: ¥<b>{point.y:.2f}</b>',
+              xDateFormat: '%m/%d'
+            },
+            legend: {
+              layout: 'vertical',
+              align: 'right',
+              verticalAlign: 'top',
+              x: -10,
+              y: 100,
+              borderWidth: 0
+            },
+            series: [{
+              type: 'line',
+              name: selectedOption,
+              data: formattedData
             }]
-          },
-          tooltip: {
-            pointFormat: 'CPM: ¥<b>{point.y:.2f}</b>',
-            xDateFormat: '%m/%d'
-          },
-          legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'top',
-            x: -10,
-            y: 100,
-            borderWidth: 0
-          },
-          series: [{
-            type: 'line',
-            name: selectedOption, //選択されたオプション（＝チャート名）
-            data: formattedData  //変換後のデータ
-          }]
-        });
-        
+          });
+          setChart(newChart);
+        }
+
       } catch (error) {
         console.error(`Error fetching data: ${error}`);
       }
     }
-  
-    fetchData();
-  }, [selectedOption]); //selectedOptionが変わる度にuseEffectが発火します。
 
-  // select要素が変更されたときにselectedOptionを更新　→ selectedOptionが変更されるとuseEffectでAPIリクエストが行われhighCharts描画が行われる
+    fetchData();
+  }, [selectedOption]);
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/loggedin', { withCredentials: true });
+        setLoggedIn(response.data.loggedIn);
+      } catch (error) {
+        console.error(`Error checking login status: ${error}`);
+      }
+    }
+
+    checkLoginStatus();
+  }, []);
+
   const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedOption(event.target.value);
-};
+  };
+
+  const handleLogin = () => {
+    window.location.href = "http://localhost:5000/login";
+  }
+
+  const handleLogout = async () => {
+    if (chart !== null) {
+      chart.destroy();
+    }
+    await axios.get('http://localhost:5000/logout', { withCredentials: true });
+    setLoggedIn(false);
+  }
+
+  if (!loggedIn) {
+    return (
+      <div className="center">
+        <button onClick={handleLogin}>ログイン</button>
+      </div>
+    );
+  }
 
   return (
     <>
-      <div className="App" id="container">
-      </div>
+      <div className="App" id="container" ref={containerRef}></div>
       <div className="center">
         <select value={selectedOption} onChange={handleChange}>
           <option value="Overlay">Overlay</option>
           <option value="Interstitial">Interstitial</option>
-          <option value="Inarticle">Inarticle</option>
         </select>
+        <button onClick={handleLogout}>ログアウト</button>
       </div>
     </>
   );
