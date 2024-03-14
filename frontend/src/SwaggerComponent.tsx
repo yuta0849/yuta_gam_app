60664bb48e0f0759c7a95d22fb67882e35d9ff3c
Yuta <yuta.marines.team26l@gmail.com>
2024-03-15 02:34:51 +0900

    環境変数を利用したSwaggerページのルーティングが完了、開発環境での表示確認完了

--- frontend/src/SwaggerComponent.tsx
+++ frontend/src/SwaggerComponent.tsx
@@ -3,8 +3,9 @@ import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
 
 const SwaggerComponent = () => {
-  return (
-    <SwaggerUI url="path-to-your-openapi-spec-file.yaml" />
+    const apiUrl = process.env.REACT_APP_SWAGGER_API_URL;
+    return (
+      <SwaggerUI url={apiUrl} />
   );
 };
 
