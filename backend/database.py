615752b02d780680b347636872877beda9e73c9b
Yuta <yuta.marines.team26l@gmail.com>
2024-03-18 00:30:11 +0900

    本番環境にusersテーブルを作成

--- backend/database.py
+++ backend/database.py
@@ -2,6 +2,8 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.declarative import declarative_base
+from models import User
+
 
 user = os.environ['MYSQL_USER']
 password = os.environ['MYSQL_PASSWORD']
@@ -9,10 +11,14 @@ host = os.environ['MYSQL_HOST']
 dbname = os.environ['MYSQL_DATABASE']
 
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{user}:{password}@{host}/{dbname}"
-print(SQLALCHEMY_DATABASE_URL)
+# print(SQLALCHEMY_DATABASE_URL)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-SessionLocal: Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
+
+# FLASK_ENVがproductionだった場合にのみテーブルを作成(開発環境ではdockercontainer内でSQLを実行している)
+if os.getenv('FLASK_ENV') == 'production':
+    Base.metadata.create_all(bind=engine, tables=[User.__table__])
+    
+SessionLocal: Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
